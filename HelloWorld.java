80bdb86bc16bd400aca645cdbc3e8a38d9a050f3
sandeepkakkar <153259318+Sandeep-Kakkar@users.noreply.github.com>
2024-02-09 11:38:53 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld
 {
     public static void main(String args[])
     {
-        System.out.println("Hello Sandy");
+        System.out.println("Hello Sandy")
         System.out.println("Java for Jenkins, Slack and Ngrok");
     }
 }  
